feb5a098d8ba4990634467b9f01f9db9ba3c20cd
anoopraju31 <anoopraju31@gmail.com>
2023-10-20 12:51:02 +0530

    Welcome Screen - styled

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,10 +1,40 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { Image, Text, View } from 'react-native'
+import {
+	widthPercentageToDP as wp,
+	heightPercentageToDP as hp,
+} from 'react-native-responsive-screen'
 
 const WelcomeScreen = () => {
 	return (
-		<View className='flex-1 justify-center items-center bg-green-500'>
-			<Text>WelcomeScreen</Text>
+		<View className='flex-1 justify-center items-center bg-amber-500'>
+			<StatusBar style='light' />
+
+			{/* Logo */}
+			<View className='bg-white/20 rounded-full' style={{ padding: hp(5.5) }}>
+				<View className='bg-white/20 rounded-full' style={{ padding: hp(5) }}>
+					<Image
+						source={require('../../assets/images/welcome.png')}
+						style={{ width: hp(20), height: hp(20) }}
+					/>
+				</View>
+			</View>
+
+			{/* Title and Hero Text */}
+			<View className='flex items-center space-y-2'>
+				<Text
+					style={{ fontSize: hp(7) }}
+					className='font-bold text-white tracking-widest'>
+					{' '}
+					Foody{' '}
+				</Text>
+				<Text
+					style={{ fontSize: hp(2.5) }}
+					className='font-medium text-white tracking-widest'>
+					{' '}
+					Food is always right!{' '}
+				</Text>
+			</View>
 		</View>
 	)
 }
