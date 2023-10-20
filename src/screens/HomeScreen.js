f1d2651a7c8665f25fbb7d8c83ea325a6a6bf9f2
anoopraju31 <anoopraju31@gmail.com>
2023-10-20 14:03:33 +0530

    Home Screen - Avatar & Bell icon added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,29 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { Image, ScrollView, Text, TextInput, View } from 'react-native'
+import {
+	widthPercentageToDP as wp,
+	heightPercentageToDP as hp,
+} from 'react-native-responsive-screen'
+import { BellIcon } from 'react-native-heroicons/outline'
 
 const HomeScreen = () => {
 	return (
-		<View className='flex-1 justify-center items-center bg-green-500'>
-			<Text>HomeScreen</Text>
+		<View className='flex-1 bg-white'>
+			<StatusBar style='dark' />
+
+			<ScrollView
+				showsVerticalScrollIndicator={false}
+				contentContainerStyle={{ paddingBottom: 50 }}
+				className='space-y-6 pt-14'>
+				{/* Avatar & Bell icon */}
+				<View className='mx-4 flex-row justify-between items-center mb-2'>
+					<Image
+						source={require('../../assets/images/avatar.png')}
+						style={{ width: hp(5.5), height: hp(5) }}
+					/>
+					<BellIcon size={hp(4)} color='gray' />
+				</View>
+			</ScrollView>
 		</View>
 	)
 }
