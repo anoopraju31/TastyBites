c503c39a1aec9e8ad94c1aaf8f4cb9992f8de260
anoopraju31 <anoopraju31@gmail.com>
2023-10-20 17:11:03 +0530

    Loading Component added

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Categories } from './Categories'
 export { default as Recipes } from './Recipes'
 export { default as RecipeCard } from './RecipeCard'
+export { default as Loading } from './Loading'
