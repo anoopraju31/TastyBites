6f9596c7e9bbf1b7bd7e901d9764de2cf9d980ea
anoopraju31 <anoopraju31@gmail.com>
2023-10-20 12:20:12 +0530

    Tailwind CSS setup

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View className='flex-1 items-center justify-end bg-black'>
+			<Text className='font-bold text-xl p-4 bg-orange-300 text-center'>
+				Open up App.js
+			</Text>
+			<StatusBar style='auto' />
+		</View>
+	)
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
