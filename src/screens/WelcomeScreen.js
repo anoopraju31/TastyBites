fe036f2d531942f3f66ee6b95490f5f5546c751d
anoopraju31 <anoopraju31@gmail.com>
2023-10-20 12:24:38 +0530

    Welcome Screen - initial

---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const WelcomeScreen = () => {
+	return (
+		<View className='flex-1 justify-center items-center bg-green-500'>
+			<Text>WelcomeScreen</Text>
+		</View>
+	)
+}
+
+export default WelcomeScreen
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,1 +1,2 @@
 export { default as HomeScreen } from './HomeScreen'
+export { default as WelcomeScreen } from './WelcomeScreen'
