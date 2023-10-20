13d82a47e163e122f530f40fe8e8a547f13cc2ab
anoopraju31 <anoopraju31@gmail.com>
2023-10-20 12:23:16 +0530

    Home Screen - initial

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const HomeScreen = () => {
+	return (
+		<View className='flex-1 justify-center items-center bg-green-500'>
+			<Text>HomeScreen</Text>
+		</View>
+	)
+}
+
+export default HomeScreen
