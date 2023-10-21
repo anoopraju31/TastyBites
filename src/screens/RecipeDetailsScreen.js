b1bc9cb40b3ca590836461642c6aaa022434cecd
anoopraju31 <anoopraju31@gmail.com>
2023-10-21 18:12:53 +0530

    Recipe Instruction added

--- src/screens/RecipeDetailsScreen.js
+++ src/screens/RecipeDetailsScreen.js
@@ -176,6 +176,23 @@ const RecipeDetailsScreen = (props) => {
 							))}
 						</View>
 					</Animated.View>
+
+					{/* instructions */}
+					<Animated.View
+						entering={FadeInDown.delay(300)
+							.duration(700)
+							.springify()
+							.damping(12)}
+						className='space-y-4'>
+						<Text
+							style={{ fontSize: hp(2.5) }}
+							className='font-bold flex-1 text-neutral-700'>
+							Instructions
+						</Text>
+						<Text style={{ fontSize: hp(1.6) }} className='text-neutral-700'>
+							{meal?.strInstructions}
+						</Text>
+					</Animated.View>
 				</View>
 			)}
 		</ScrollView>
